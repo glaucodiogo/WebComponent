6d69950066f852a63c5fb3a1abd3e8ac6af3fb0c
Glauco Diogo <glauco.oliveira@load.digital>
2022-12-08 00:52:45 -0300

    Refactor comp cardNews

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -3,11 +3,46 @@ class CardNews extends HTMLElement{
         super();//chama o construtor pai 
 
 
-        const shadow = this.attachShadow({
-            mode:"open"
-        });
+        const shadow = this.attachShadow({mode:"open"});
 
-        shadow.innerHTML = "<h1>Hello world</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
+    }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class","card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class","card_left");
+
+        const autor = document.createElement("span");
+        autor.textContent= "By " + (this.getAttribute("autor") || "Anonimo");
+        
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = " "+this.getAttribute("title");
+        const newsContent = document.createElement("p");
+        newsContent.textContent = " "+this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class","card_right");
+
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+
+            //add childs to componenRoot father
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+    style(){
+
     }
 }
 
