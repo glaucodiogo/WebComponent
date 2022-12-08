2d789ef7e99bcdceb8626dcb75ed9f0caaa0572e
Glauco Diogo <glauco.oliveira@load.digital>
2022-12-08 00:25:16 -0300

    Add prop titulo for component TituloDinamico.js

--- src/Components/TituloDinamico.js
+++ src/Components/TituloDinamico.js
@@ -7,11 +7,19 @@ class TituloDinamico extends HTMLElement{
 
 
         //base do component
-
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo");
         //estilizar o comp 
-
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `;
         //enviar para a shadow 
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
     }
 }
 
-customElements.define("titulo-dinamico")
+customElements.define("titulo-dinamico",TituloDinamico);
